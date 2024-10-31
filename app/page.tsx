1953ae0c83ff1508803a09c91c80ce9f9e4a7d3d
mitchell <mitchellkellywalters@gmail.com>
2024-10-30 19:17:44 -0700

    mega menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { HeaderMenu } from "@/components/HeaderMenu";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="w-screen">
+      <HeaderMenu />
       <Image
         aria-hidden
         src="/headshot.jpeg"
