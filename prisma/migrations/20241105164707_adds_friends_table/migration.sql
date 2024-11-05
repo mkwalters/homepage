b8ab0b89263db9b3c8b85d27a8b06bf8d95d1170
mitchell <mitchellkellywalters@gmail.com>
2024-11-05 08:49:04 -0800

    adds migration

---
+++ prisma/migrations/20241105164707_adds_friends_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Friend" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Friend_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Friend_email_key" ON "Friend"("email");
